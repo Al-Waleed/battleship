389745d7be584861c1481c5ba2d86e0746eddcb0
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-06-12 00:15:41 +0300

    Create a receiveAttack function that checks if a point has a ship in it or not then it sends the hit function to that ship if it exists && add multiple test cases && edit placeShip function to keep the placed ship connected when we hit it even if its at multiple squares(points)

--- src/gameboard.js
+++ src/gameboard.js
@@ -10,10 +10,22 @@ export function createGameBoard() {
   }
 
   const placeShip = (length, ...coordinates) => {
+    // to keep the ship connected even if its at different coordinates
+    const shipToPlace = ship(length);
     coordinates.forEach((point) => {
-      board[point] = ship(length);
+      board[point] = shipToPlace;
     });
   };
 
-  return { board, placeShip };
+  const receiveAttack = (point) => {
+    if (board[point] === null) {
+      board[point] = "destroyed";
+      return board[point];
+    } else if (board[point].isSunk() === true) {
+      return "ship is already sunk";
+    } else {
+      board[point].hit();
+    }
+  };
+  return { board, placeShip, receiveAttack };
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,5 +1,4 @@
-import { createGameBoard } from "./gameboard";
-import { ship } from "./ship";
+import { createGameBoard, receiveAttack } from "./gameboard";
 
 // create the board
 const testBoard = createGameBoard();
@@ -14,4 +13,37 @@ it("places more than one ship on testBoard", () => {
   expect(testBoard.board[[1, 1]].state.length).toBe(4);
   expect(testBoard.board[[2, 2]].state.length).toBe(4);
   expect(testBoard.board[[3, 3]].state.length).toBe(4);
+});
+
+// hit the ship
+testBoard.receiveAttack([1, 1]);
+it("hits a ship", () => {
+  expect(testBoard.board[[1, 1]].state.hits).toBe(1);
+});
+
+it("hit the same ship at different points", () => {
+  expect(testBoard.board[[2, 2]].state.hits).toBe(1);
+});
+
+it("hit the same ship at different points", () => {
+  expect(testBoard.board[[3, 3]].state.hits).toBe(1);
+});
+
+it("doesn't affect other ships", () => {
+  expect(testBoard.board[[0, 0]].state.hits).toBe(0);
+});
+
+testBoard.receiveAttack([5, 5]);
+it("hits an empty square", () => {
+  expect(testBoard.board[[5, 5]]).toBe("destroyed");
+});
+
+testBoard.placeShip(1, [4, 7]);
+testBoard.receiveAttack([4, 7]);
+it("sinks", () => {
+  expect(testBoard.board[[4, 7]].isSunk()).toBe(true);
+});
+
+it("hits a sunken ship", () => {
+  expect(testBoard.receiveAttack([4, 7])).toBe("ship is already sunk");
 });
