f57023028817dc07b700c1b1969b023f1e6f2688
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-06-13 04:54:55 +0300

    Edit displayBoard function to add different classes and dataset values if the square has a ship on it or if it's null

--- src/display.js
+++ src/display.js
@@ -1,11 +1,19 @@
-export function displayBoard(player, board) {
+import createPlayer from "./player";
+
+function displayBoard(player, board) {
   const boardDiv = document.getElementById(`player-${player}-board`);
 
   for (const square in board) {
     const div = document.createElement("div");
-    div.classList.add("square");
+    div.classList.add(`player-${player}-square`);
     div.textContent = `(${square})`;
-    div.dataset.value = board[square]
+    if (board[square] !== null) {
+      div.classList.add("ship");
+      div.dataset.value = square;
+    } else {
+      div.dataset.value = board[square];
+      div.classList.add("square");
+    }
     boardDiv.append(div);
   }
 }
