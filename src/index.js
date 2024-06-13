2df73e34ddf152ccc4ebe1f05bf9c183a9a39b23
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-06-13 06:54:11 +0300

    Populate the boards with ships to see how everything looks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./style.css";
+import { playerOne, playerTwo, updatePlayerBoard } from "./display";
+import { getCurrentPlayer, switchPlayer } from "./player";
+
+playerOne.playersGameBoard.placeShip(2, [5, 8], [5, 9]);
+playerOne.playersGameBoard.placeShip(3, [2, 0], [2, 1], [2, 2]);
+playerOne.playersGameBoard.placeShip(3, [1, 6], [2, 6], [3, 6]);
+playerOne.playersGameBoard.placeShip(4, [7, 5], [7, 6], [7, 7], [7, 8]);
+playerOne.playersGameBoard.placeShip(5, [5, 2], [6, 2], [7, 2], [8, 2], [9, 2]);
+
+playerTwo.playersGameBoard.placeShip(2, [0, 0], [0, 1]);
+playerTwo.playersGameBoard.placeShip(3, [1, 4], [2, 4], [3, 4]);
+playerTwo.playersGameBoard.placeShip(3, [1, 6], [2, 6], [3, 6]);
+playerTwo.playersGameBoard.placeShip(4, [9, 0], [9, 1], [9, 2], [9, 3]);
+playerTwo.playersGameBoard.placeShip(5, [6, 0], [6, 1], [6, 2], [6, 3], [6, 4]);
+
+updatePlayerBoard();
