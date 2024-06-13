0137c228be42469f80facee9f6b98362dd887951
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-06-13 12:02:58 +0300

    Add an array to keep track of not visited squares && and start using makeComputerMove to get a random move from the computer

--- src/eventListener.js
+++ src/eventListener.js
@@ -1,6 +1,6 @@
+import makeComputerMove from "./computerPlayer";
 import { playerOne, computer, updatePlayerBoard } from "./display";
 import { getCurrentPlayer, switchPlayer } from "./player";
-
 
 function endGame(player) {
   let winner = getCurrentPlayer();
@@ -9,19 +9,25 @@ function endGame(player) {
     return true;
   }
 }
-
+// to keep track of squares that isn't visited yet by the computer
+let notVisited = [];
 export default function updateListeners() {
   const playerOneSquares = document.querySelectorAll(".player-one-square");
   const playerTwoSquares = document.querySelectorAll(".player-two-square");
-// 
+
   if (getCurrentPlayer() === "playerOne") {
+    // we empty the array to update it later
+    notVisited = [];
     playerOneSquares.forEach((square) => {
       const lastClass = square.classList[square.classList.length - 1];
       // to know if the square has been attacked or not
       if (lastClass === "ship" || lastClass === "square") {
+        notVisited.push(square);
         square.addEventListener("click", () => eventHandler(square, playerOne));
       }
     });
+    // make the computer move from the notVisited array 
+    makeComputerMove(notVisited)
   } else if (getCurrentPlayer() === "computer") {
     playerTwoSquares.forEach((square) => {
       const lastClass = square.classList[square.classList.length - 1];
