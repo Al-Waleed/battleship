dd13c237006142fbc04feaff1ade6749e8e0fcc7
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-06-13 13:35:09 +0300

    Create a function that adds ships to a random location on both of the boards

--- src/index.js
+++ src/index.js
@@ -2,17 +2,55 @@ import "./style.css";
 import { playerOne, computer, updatePlayerBoard } from "./display";
 import updateListeners from "./eventListener";
 
-playerOne.playersGameBoard.placeShip(2, [5, 8], [5, 9]);
-playerOne.playersGameBoard.placeShip(3, [2, 0], [2, 1], [2, 2]);
-playerOne.playersGameBoard.placeShip(3, [1, 6], [2, 6], [3, 6]);
-playerOne.playersGameBoard.placeShip(4, [7, 5], [7, 6], [7, 7], [7, 8]);
-playerOne.playersGameBoard.placeShip(5, [5, 2], [6, 2], [7, 2], [8, 2], [9, 2]);
-
-computer.playersGameBoard.placeShip(2, [0, 0], [0, 1]);
-computer.playersGameBoard.placeShip(3, [1, 4], [2, 4], [3, 4]);
-computer.playersGameBoard.placeShip(3, [1, 6], [2, 6], [3, 6]);
-computer.playersGameBoard.placeShip(4, [9, 0], [9, 1], [9, 2], [9, 3]);
-computer.playersGameBoard.placeShip(5, [6, 0], [6, 1], [6, 2], [6, 3], [6, 4]);
+placeShipsRandomly(playerOne);
+placeShipsRandomly(computer);
 
 updatePlayerBoard();
 updateListeners();
+
+function placeShipsRandomly(player) {
+  const options = [];
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      options.push([i, j]);
+    }
+  }
+  let index1 = Math.floor(Math.random() * options.length - 1) + 1;
+  let index2 = Math.floor(Math.random() * options.length - 1) + 1;
+  let index3 = Math.floor(Math.random() * options.length - 1) + 1;
+  let index4 = Math.floor(Math.random() * options.length - 1) + 1;
+  let index5 = Math.floor(Math.random() * options.length - 1) + 1;
+
+  player.playersGameBoard.placeShip(
+    2,
+    options.splice(index1, 1),
+    options.splice(index1, 1)
+  );
+  player.playersGameBoard.placeShip(
+    3,
+    options.splice(index2, 1),
+    options.splice(index2, 1),
+    options.splice(index2, 1)
+  );
+  player.playersGameBoard.placeShip(
+    3,
+    options.splice(index3, 1),
+    options.splice(index3, 1),
+    options.splice(index3, 1)
+  );
+  player.playersGameBoard.placeShip(
+    4,
+    options.splice(index4, 1),
+    options.splice(index4, 1),
+    options.splice(index4, 1),
+    options.splice(index4, 1)
+  );
+  player.playersGameBoard.placeShip(
+    5,
+    options.splice(index5, 1),
+    options.splice(index5, 1),
+    options.splice(index5, 1),
+    options.splice(index5, 1),
+    options.splice(index5, 1)
+  );
+}
